4a7d4bc3c933d3bfbc583bcb7352de707428dca5
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2022-11-20 11:40:14 -0600

    Agregar archivos de proyecto.

---
+++ SeguridadWeb.AccesoADatos/BDContexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ********************************
+using Microsoft.EntityFrameworkCore;
+using SeguridadWeb.EntidadesDeNegocio;
+
+namespace SeguridadWeb.AccesoADatos
+{
+    public class BDContexto: DbContext
+    {
+        public DbSet<Rol> Rol { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=tcp:server20221120.database.windows.net,1433;Initial Catalog=db2022;Persist Security Info=False;User ID=adminsql;Password=WKxTn9WEMpteA59;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
---
+++ SeguridadWeb.WebAPI/Auth/IJwtAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿// agregar la siguiente libreria
+using SeguridadWeb.EntidadesDeNegocio;
+namespace SeguridadWeb.WebAPI.Auth
+{
+    public interface IJwtAuthenticationService
+    {
+        string Authenticate(Usuario pUsuario);
+    }
+}
